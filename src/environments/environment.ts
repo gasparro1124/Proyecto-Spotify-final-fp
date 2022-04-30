4064ff584bb7b2b490b6a0ee375dbad68ca4e1e6
gasparro1124 <albgas.bon@gmail.com>
2022-04-30 11:24:39 +0200

    first commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,6 +6,29 @@ export const environment = {
   production: false
 };
 
+export const SpotifyConfiguration = {
+  clientId: '628ee370ea4c43bc804dc3771cedb07d',
+  authEndpoint: 'https://accounts.spotify.com/authorize',
+  redirectUrl: 'http://localhost:4200/login/',
+  scopes: [
+    "user-read-currently-playing", //musica actual.
+    "user-follow-modify", //permite que el usuario modifique si sigue o no
+    'user-read-playback-position',//permite modificar la posicion del playback
+    "user-read-recently-played", // musica receinte
+    "user-read-playback-state", // saber la musica reciente
+    "user-top-read", // top artistas
+    'user-follow-read',//acceso  a artistas que sigo
+    'playlist-modify-public',//permite modificar, seguir playlist
+    'user-library-modify',//permite modificar albunes, canciones guaradas
+    "user-modify-playback-state", // top artistas
+    "user-library-read", // leer libreria usurio
+    "playlist-read-private", // ver playlist privadas
+    "playlist-read-collaborative", // ver playlist compartidas
+    "user-library-read",//Da acceso a la libreria del usuario
+    "playlist-modify-private",//permite modificar playlist privadas
+  ]
+}
+
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
